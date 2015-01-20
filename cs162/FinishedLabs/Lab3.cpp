3d2d2494d682c28c47009d590eeda956cbbd5138
Maker23 <shoshana.abrass@gmail.com>
2015-01-20 12:55:55 -0800

    Adding Lab3.  File IO.

---
+++ cs162/FinishedLabs/Lab3.cpp
@@ -0,0 +1,350 @@
+/* vim:ts=2
+ *
+ * CS162, Lab3:  File I/O
+ *
+ * Merge two files that each contains sorted integers
+ * Write the results to a third file
+ *
+ * Input files should be read one line at a time.
+ *
+ * Design choices:
+ * 		Silently supress duplicates
+ * 		Warn and continue if bad data is found
+ * 		If one or both files are empty that's not an error; silently proceed
+ * 		Command-line arguments are positional, not flag-defined
+ *
+ * Test scenarios:
+ *  One or both files does not exist; program exits
+ *  One or both files exists but not permitted to read; program exits
+ *  One or both files exist but are empty
+ *  One file is much shorter than the other
+ *  Numbers are both space delimited and CRLF delimited
+ *  Bad (non-integer) values exist (strings, decimal numbers)
+ *  Duplicate numbers occur in the same file
+ *  Duplicate numbers are found in fileOne and fileTwo
+ *  Swap input files one and two, output should be the same
+ *
+ */
+#include <fstream> 	// File I/O
+#include <iostream>
+#include <string>
+#include <cstring>	// Because  strlen
+#include <climits>  // INT_MAX, INT_MIN
+
+#define DEBUG false
+
+#define FILE_ONE "infile.one" // must be a file of sorted integers
+#define FILE_TWO "infile.two" // must be a file of sorted integers
+#define OUTPUT_FILE "outfile"
+
+void SortTwoIntoOne(std::ifstream &inputStreamOne, std::ifstream &inputStreamTwo, std::ofstream &outputStream);
+void ReadOneInt(std::ifstream &Input, int ** Value, std::string Filename);
+int validateIntegerInput(const char* inputString, int MinValue, int MaxValue);
+
+/****************************************************************************
+ * main()
+ *
+ * Check the command-line arguments (file names);
+ * error and exit if the files are bad.
+ *
+ ***************************************************************************/
+
+int main(int argc, char **argv) 
+{
+	if ( argc < 4 ) 
+	{
+		std::cout << "Too few arguments (" << argc << ")" <<std::endl;
+		std::cout << "USAGE:  <print usage message here>" << std::endl;
+		//if (DEBUG) 
+		//{
+			for (int i=0;i<argc;i++)
+			{
+				std::cout << "Arg #" << i <<" = " << argv[i] << std::endl;
+			}
+		//}
+		exit(-1);
+	}
+	char *InputFileOne = argv[1];
+	char *InputFileTwo = argv[2];
+	char *OutputFile = argv[3];
+	std::ifstream inputStreamOne(InputFileOne);
+	std::ifstream inputStreamTwo(InputFileTwo);
+	std::ofstream outputStream(OutputFile); 
+			// If this were a real program we'd make sure the input files 
+			// were good before we open/truncate the output file.
+
+  
+	/* Exit if any of the files were unopeneable */
+	if (! inputStreamOne.good()) 
+	{
+		std::cout << "Error: Could not open file \""<< InputFileOne << "\" for read; exiting..."<<std::endl;
+		exit(-1);
+	}
+	if (! inputStreamTwo.good()) 
+	{
+		std::cout << "Error: Could not open file \""<< InputFileTwo <<"\" for read; exiting..."<<std::endl;
+		exit(-1);
+	}
+	if (! outputStream.good()) 
+	{
+		std::cout << "Error: Could not open file \""<< InputFileTwo <<"\" for write; exiting..."<<std::endl;
+		exit(-1);
+	}
+	SortTwoIntoOne(inputStreamOne, inputStreamTwo, outputStream);
+}
+/****************************************************************************
+ *
+ *
+ *
+ *
+ *
+ ***************************************************************************/
+
+void SortTwoIntoOne(std::ifstream &inputStreamOne, std::ifstream &inputStreamTwo, std::ofstream &outputStream)
+{
+	char *InputFileOne = "foo";
+	char *InputFileTwo = "bar";
+	char *OutputFile = "baz";
+	int *inputOne = new (int);
+	int *inputTwo = new (int);
+	int *prevInputOne = new int (INT_MIN); 
+	int *prevInputTwo = new int (INT_MIN);
+
+
+	ReadOneInt(inputStreamOne, &inputOne, InputFileOne);
+	ReadOneInt(inputStreamTwo, &inputTwo, InputFileTwo);
+	while ( inputOne && inputTwo )
+	{
+		// Good values from both files
+		if ( *inputOne < *inputTwo) 
+		{
+			if (DEBUG) std::cout << "Writing inputOne, recycling One" 
+				<< "	" << *inputOne << std::endl;
+			if (*inputOne != *prevInputOne)
+			{
+				outputStream << *inputOne << std::endl;
+				*prevInputOne = *inputOne;
+			}
+			ReadOneInt(inputStreamOne, &inputOne, InputFileOne);
+		}
+		else if (*inputOne > *inputTwo)
+		{
+			if (DEBUG) std::cout << "Writing inputTwo, recycling Two" 
+				<< "	" << *inputTwo << std::endl;
+			if (*inputTwo != *prevInputTwo)
+			{
+				outputStream << *inputTwo << std::endl;
+				*prevInputTwo = *inputTwo;
+			}
+			ReadOneInt(inputStreamTwo, &inputTwo, InputFileTwo);
+		}
+		else if (*inputOne == *inputTwo) 
+		{
+			if (DEBUG) std::cout << "Writing inputOne, recycling both" 
+				<< "	" << *inputOne << std::endl
+				<< "	" << *inputTwo << std::endl;
+			if ((*inputOne != *prevInputOne)
+			   &&(*inputTwo != *prevInputTwo))
+			{
+				outputStream << *inputOne << std::endl;
+				*prevInputOne = *inputOne;
+				*prevInputTwo = *inputTwo;
+			}
+			ReadOneInt(inputStreamOne, &inputOne, InputFileOne);
+			ReadOneInt(inputStreamTwo, &inputTwo, InputFileTwo);
+		}
+	}
+	// One or both file descriptors are NULL
+  if ( ! (inputOne || inputTwo) ) 
+	{ // Nothing more to do here :)
+	}
+	else if ( ! inputOne )
+	{
+		// InputFileOne has no more valid numbers in it
+		while ( inputTwo)
+		{
+			if (DEBUG) std::cout << "Writing inputTwo, recycling Two" 
+				<< "	" << *inputTwo << std::endl;
+			outputStream << *inputTwo << std::endl;
+			ReadOneInt(inputStreamTwo, &inputTwo, InputFileTwo);
+		}
+	}
+	else if ( ! inputTwo )
+	{
+		// InputFileTwo has no more valid numbers in it
+		while ( inputOne)
+		{
+			if (DEBUG) std::cout << "Writing inputOne, recycling One" 
+				<< "	" << *inputOne << std::endl;
+			outputStream << *inputOne << std::endl;
+			ReadOneInt(inputStreamOne, &inputOne, InputFileOne);
+		}
+	}
+}
+
+void ReadOneInt(std::ifstream &Input, int ** Value, std::string Filename)
+{
+	if (DEBUG) std::cout << "Entering ReadOneWord" << std::endl;
+
+	std::string Line;
+	int tmpInt;
+
+	if ( Input.eof() )
+	{
+		if (DEBUG) std::cout << "I think I'm setting the pointr to Null" << std::endl;
+		*Value = NULL;
+		return;
+	}
+	else if ( Input.good() )
+	{
+		do
+		{
+	  	Input >> Line;
+			tmpInt = validateIntegerInput(Line.c_str(), INT_MIN + 1, INT_MAX);
+			if (tmpInt == INT_MIN)
+			{
+				// Skip silently unless we're debugging
+				if (DEBUG) 
+				{
+					std::cout << "Warning: bad input found in file " << Filename << std::endl;
+			 		std::cout << "getline = \"" << Line << "\"" << std::endl;
+					std::cout << "validate = \"" << tmpInt << "\"" << std::endl;
+				}
+			}
+		  if (DEBUG) std::cout << "tmpInt = " << tmpInt << std::endl;
+		}
+		while ( Input.good() && tmpInt == INT_MIN);
+		if (tmpInt != INT_MIN)
+		{
+			**Value = tmpInt;
+			return;
+		}
+		else 
+		{
+			if (DEBUG) std::cout << "I think I'm seeting the pointr to Null" << std::endl;
+			*Value = NULL;
+			return;
+		}
+	}
+	else
+	{
+		std::cout << "ERROR: Can't read input from "<< Filename <<std::endl;
+		Value = NULL;
+		return;
+	}
+
+	if (DEBUG) std::cout << "End of ReadOneWord - we should never get here" << std::endl;
+}
+
+#define ZERO 48
+#define NINE 57
+#define DASH 45
+#define DOT 46
+#define SPACE 32
+
+/* ***************************************************************************
+ *  validateIntegerInput (InputString, MinValue, MaxValue);
+ *
+ *  Purpose: Confirm that a string is a valid integer number
+ *  Input: 
+ *  	InputString:  char* c-string
+ *  	MinValue: the low end of the valid number range
+ *  	Maxvalue: the high end of the valid number range
+ *
+ *  Note that acceptable integers
+ *  	* Can begin with a minus sign (negative)
+ *  	* Can not have any characters other than [0-9] and "."
+ *  		* If "." appears, test the string as a float. It may be a number.00;
+ *  		  this is accepted as an integer by this function (eg, 43.0,  109.00)
+ *
+ * ***************************************************************************/
+
+int validateIntegerInput(
+	const char* inputString,
+	int MinValue,
+	int MaxValue)
+{
+	double inputDouble = 0.0; 	// The input string, as a double
+	int  length_of_input = 0;		// Length of the users's input string
+	int  inputInteger = 0;			// The input string, as an integer
+	int  MaxTries = 1;
+	int  number_of_tries = 0;		// Number of times user has tried to enter data
+	char buffer[256];						// A buffer for creating the prompt string
+	bool notANumber = false;
+	//std::string inputCopy;
+
+	if (DEBUG) std::cout << "_validateIntegerInput function two" << std::endl;
+	
+	do {
+	  number_of_tries++;
+
+		// Confirm that all the character in the input string are legal 
+		// as part of a number
+		length_of_input = std::strlen(inputString);
+		if (length_of_input == 0 ) 
+		{
+			if (DEBUG) std::cout << "Whoa nellie! no input??" << std::endl;
+			return(MinValue-1);
+		}
+		for (int i=0; i < length_of_input; i++) 
+		{
+			// Digits [0-9], negative signs, and decimal points are ok
+			// We should really strip spaces
+			if(!( ((inputString[i] >= ZERO) && (inputString[i] <= NINE ))
+						|| ( i == 0 && inputString[i]==DASH)
+						|| ( i == 0 && inputString[i]==SPACE)
+						|| ( i == length_of_input - 1 && inputString[i]==SPACE)
+						|| ( inputString[i] == DOT )
+					))
+			{
+				// THIS IS NOT A NUMBER
+				// TODO: Support Octal, binary, and Hex
+				notANumber = true;
+			}
+		}
+
+		// Now that we know the string is a number, convert it to a double
+		// Copy the double into an integer; then test if they are equal
+		//inputCopy = inputString;
+		//std::stringstream ss (inputCopy); // stringstream is not allowed
+		//ss >> inputDouble;
+		inputDouble = (double)atof(inputString);
+		inputInteger = inputDouble; // if inputDouble is a floating point value,
+		                            // inputInteger will be truncated
+																// and the will not be equal when tested below
+		if (DEBUG) std::cout << "inputDouble = " << inputDouble << ", inputInteger = " 
+			<< inputInteger << std::endl;
+
+		if ( notANumber )
+		{
+			// std::cout << "ERROR: Part of your input was not a number\n";
+			// TODO 'non-numeric character X found'
+		}
+		else if ( inputDouble != static_cast<double>(inputInteger) )
+		{
+				// Number is a float with a fractional part
+				// TODO: this is subject to the accuracy limits of the double, 
+				// for example, 10.0000000000000001  is equal to 10.
+				// We could instead warn when a decimal point is detected...
+				std::cout << "ERROR: Number contained a decimal value\n";
+		}
+	  else if ( (inputInteger < MinValue ) || (inputInteger > MaxValue) ) 
+		{
+ 			if ( DEBUG ) {std::cout << "Numeric value l.181 = " << inputInteger 
+				<< "num_tries = " << number_of_tries << "\n";}
+   		std::cout << "ERROR: Value " << inputInteger << " is outside permitted range ["
+				<< MinValue << "-" << MaxValue << "]" << std::endl;
+			return(MinValue-1);
+		}
+		else
+		{
+					return(inputInteger);
+		}
+		notANumber = false;  // reset
+	}
+	while ( !number_of_tries || (number_of_tries < MaxTries) );
+
+	std::cout << "Warning: \"" << inputString << "\" is not an integer value; skipping..." <<std::endl;
+	return(MinValue-1);	 // Hard to return an error code
+}
+
